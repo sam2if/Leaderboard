a5c9f988bce60ca7ebb2e2e024cbc285db2bd460
sami <samif3443@gmail.com>
2023-04-19 03:22:47 -0700

    Fix linter errors

--- src/module/add.js
+++ src/module/add.js
@@ -1,35 +1,32 @@
-
 const scoreList = document.querySelector('.score-list');
 const names = document.querySelector('.name');
 const scores = document.querySelector('.score');
 
-let players = [];
+const players = [];
 
 class Scores {
-    constructor(name, score) {
-        this.name = name.value;
-        this.score = score.value;
-    }
-}
-
-function added() {
-    let player = new Scores(names, scores);
-    if (names.value === '' || scores.value === '') {
-        alert("input name and scores");
-    }
-    else {
-        players.push(player);
-        display(player);
-        names.value = '';
-        scores.value = '';
-    }
-    
+  constructor(name, score) {
+    this.name = name.value;
+    this.score = score.value;
+  }
 }
 
 function display(player) {
-    const lists = document.createElement('div');
-    lists.innerHTML = `<li class="disboards">${player.name}: ${player.score}</li>`
-    scoreList.appendChild(lists);
+  const lists = document.createElement('div');
+  lists.innerHTML = `<li class="disboards">${player.name}: ${player.score}</li>`;
+  scoreList.appendChild(lists);
 }
 
-export { added };
+function added() {
+  const player = new Scores(names, scores);
+  if (names.value === '' || scores.value === '') {
+    alert('input name and scores');
+  } else {
+    players.push(player);
+    display(player);
+    names.value = '';
+    scores.value = '';
+  }
+}
+
+export { added, players };
