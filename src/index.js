df8a16e9cac47b36bb211b3c72e9dd3b9a076457
sami <samif3443@gmail.com>
2023-04-20 13:53:51 -0700

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -8,8 +8,13 @@ const refresh = document.getElementById('refresh');
 const form = document.getElementById('form');
 const gameApi = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/GDUN7EKtNyCRpmru02cT/scores';
 
-refresh.addEventListener('click', () => {
-  getScores(gameApi).then((data) => showScores(data.result));
+refresh.addEventListener('click', async () => {
+  try {
+    const data = await getScores(gameApi);
+    showScores(data.result);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 form.addEventListener('submit', (e) => {
